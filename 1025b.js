607053c16b882f16af4acf3eb6274c4d83b71e7a
awhnoes <133744181+awhnoes@users.noreply.github.com>
2023-10-26 10:04:07 +0900

    Add files via upload

---
+++ 1025b.js
@@ -0,0 +1,19 @@
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const result = fruits.join(',');
+    console.log(result);
+}
+
+{
+    const fruits = 'apple, kiwi, banana, cherry';
+    const result = fruits.split(',');
+    console.log(result);
+}
+
+const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 66),
+    new Student('E', 18, true, 88),
+];
